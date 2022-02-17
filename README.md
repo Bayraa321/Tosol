44e7a89f5126a3326bc1041c45346e01115d7159
Bayrmagnai Batsaihan <bayrmagnaibatsaihan@Bayrmagnais-MacBook-Pro.local>
2022-02-17 15:00:04 +0800

    hi

---
+++ README.md
@@ -0,0 +1,23 @@
+## Tosol
+
+# Run
+
+installation
+
+'''
+npm i 
+'''
+
+Start
+'''
+
+node index.js
+'''
+
+```
+git status
+git add name
+git commit -m "text"
+git push
+git pull
+```
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const port = 8080;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!!!')
+})
+
+console.log(`App is listening on port http://localhost:${port}!`)
+(port);
+
+// console.log('App is listening on port http://localhost:' + port + '!')
+
+app.listen(3000)
